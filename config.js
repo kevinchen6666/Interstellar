592f85b95c78d42d2ae164535996877dc5849848
kevinchen6666 <126632161+kevinchen6666@users.noreply.github.com>
2024-10-16 17:10:56 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,31 @@
-const config = {
-  challenge: true, // Set to true if you want to enable password protection.
-  blocked: {
-    // Normal users can ignore this option. This is for the owner of the website.
-    "gointerstellar.app": "",
-  },
-  users: {
-    // You can add multiple users by doing username: 'password'.
-    kevin: "ybcg7re8",
-  },
-};
+const config = {  
+  challenge: true, // Set to true if you want to enable password protection.  
+  blocked: {  
+    // Normal users can ignore this option. This is for the owner of the website.  
+    "gointerstellar.app": "",  
+  },  
+  users: {  
+    kevin: "ybcg7re8",  
+    kevin2: "3ybg6u9f",  
+    kevin3: "2wbt3r5x",  
+    kevin4: "n2t6y8kz",  
+    kevin5: "z6s3h4u1",  
+    kevin6: "a1g5b9y2",  
+    kevin7: "t8v6j3w4",  
+    kevin8: "q5w8u1h7",  
+    kevin9: "f4z1e6r8",  
+    kevin10: "m2s6y3n5",  
+    kevin11: "s5m8q2b7",  
+    kevin12: "j7p1w9e3",  
+    kevin13: "d6k4r3t2",  
+    kevin14: "b9u3v7h1",  
+    kevin15: "g2y1t5m6",  
+    kevin16: "x4k2j8q7",  
+    kevin17: "c3m1y5k8",  
+    kevin18: "p7t6b4f1",  
+    kevin19: "l8r3u9w2",  
+    kevin20: "j9f5t1v3",  
+  },  
+};  
 
 export default config;
